cffe04e87de1ac3ed0b1e243c000a862f0f6781b
Adithya K <a.dty.3403@gmail.com>
2021-12-25 16:50:44 +0530

    updated SLL

--- INTERNALS/sll.cpp
+++ INTERNALS/sll.cpp
@@ -1,10 +1,10 @@
-// SINGLY LINKED LIST
-#include <iostream>
+// SLL
+#include <bits/stdc++.h>
 using namespace std;
 class Node {
 public:
     int data;
-    Node *next;
+    Node* next;
     Node(int d) {
         data = d;
         next = NULL;
@@ -15,35 +15,29 @@ public:
 };
 class SLL {
 public:
-    Node *hptr = NULL;
-    void insertAtEnd(int d) {
-        Node *temp = new Node(d);
-        if (hptr == NULL) {
-            hptr = temp;
+    Node* head;
+    SLL() {
+        head = NULL;
+    }
+    void insertatend(int d) {
+        Node* temp = new Node(d);
+        if (head == NULL) {
+            head = temp;
         } else {
-            Node *ptr = hptr;
-            while (ptr->next != NULL) {
-                ptr = ptr->next;
+            Node* temp2 = head;
+            while (temp2->next != NULL) {
+                temp2 = temp2->next;
             }
-            ptr->next = temp;
+            temp2->next = temp;
         }
     }
-    void display() {
-        Node *t = hptr;
-        cout << "Elements are : ";
-        while (t != NULL) {
-            t->display();
-            t = t->next;
-        }
-        cout << endl;
-    }
-    void deletenode(int v) {
-        Node *ptr = hptr;
-        Node *prev = NULL;
+    void deletenode(int x) {
+        Node* ptr = head;
+        Node* prev = NULL;
         while (ptr != NULL) {
-            if (ptr->data == v) {
+            if (ptr->data == x) {
                 if (prev == NULL) {
-                    hptr = ptr->next;
+                    head = ptr->next;
                 } else {
                     prev->next = ptr->next;
                 }
@@ -52,29 +46,35 @@ public:
             ptr = ptr->next;
         }
     }
+    void display() {
+        Node* n = head;
+        cout << "Elements are : ";
+        while (n != NULL) {
+            n->display();
+            n = n->next;
+        }
+        cout << endl;
+    }
 };
-
 int main() {
-    SLL l;
-    int x, ch, ele, pos;
-    cout << "\nSINGLY LINKED LIST OPERATIONS" << endl;
+    SLL s;
+    int ch, d, x;
     while (1) {
-        cout << "\n1)Insert At End\n2)Delete node\n3)Display\n4)Exit" << endl;
-        cout << "Enter choice : ";
+        cout << "\n1. Insert element\n2. Delete element\n3. Display\n4. Exit\nEnter your choice : ";
         cin >> ch;
         switch (ch) {
             case 1:
-                cout << "Enter a value : ";
-                cin >> x;
-                l.insertAtEnd(x);
+                cout << "Enter element to insert at the end : ";
+                cin >> d;
+                s.insertatend(d);
                 break;
             case 2:
-                cout << "Enter a value : ";
-                cin >> ele;
-                l.deletenode(ele);
+                cout << "Enter element to delete : ";
+                cin >> x;
+                s.deletenode(x);
                 break;
             case 3:
-                l.display();
+                s.display();
                 break;
             case 4:
                 exit(0);
@@ -82,55 +82,4 @@ int main() {
                 cout << "Invalid choice" << endl;
         }
     }
-    return 0;
 }
-
-// SINGLY LINKED LIST OPERATIONS
-
-// 1)Insert At End
-// 2)Delete node
-// 3)Display
-// 4)Exit
-// Enter choice : 1
-// Enter a value : 5
-
-// 1)Insert At End
-// 2)Delete node
-// 3)Display
-// 4)Exit
-// Enter choice : 1
-// Enter a value : 10
-
-// 1)Insert At End
-// 2)Delete node
-// 3)Display
-// 4)Exit
-// Enter choice : 1
-// Enter a value : 15
-
-// 1)Insert At End
-// 2)Delete node
-// 3)Display
-// 4)Exit
-// Enter choice : 3
-// Elements are : 5 10 15
-
-// 1)Insert At End
-// 2)Delete node
-// 3)Display
-// 4)Exit
-// Enter choice : 2
-// Enter a value : 10
-
-// 1)Insert At End
-// 2)Delete node
-// 3)Display
-// 4)Exit
-// Enter choice : 3
-// Elements are : 5 15
-
-// 1)Insert At End
-// 2)Delete node
-// 3)Display
-// 4)Exit
-// Enter choice : 4
