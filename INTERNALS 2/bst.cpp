2007e29621d42ca31f355526d0b02930a2622e03
Adithya K <a.dty.3403@gmail.com>
2022-01-28 22:21:37 +0530

    added new files

---
+++ INTERNALS 2/bst.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node {
+public:
+    int data;
+    Node *left, *right;
+    Node(int d) {
+        data = d;
+        left = right = NULL;
+    }
+};
+class BST {
+public:
+    Node *root = NULL;
+    Node *addNode(Node *r, int d) {
+        if (r == NULL) {
+            Node *nptr = new Node(d);
+            return nptr;
+        } else if (r->data > d) {
+            r->left = addNode(r->left, d);
+            return r;
+        } else {
+            r->right = addNode(r->right, d);
+            return r;
+        }
+    }
+    // traversal
+    void inorder(Node *r) {
+        if (r == NULL) {
+            return;
+        }
+        inorder(r->left);
+        cout << r->data << " ";
+        inorder(r->right);
+    }
+    // height
+    int height(Node *r) {
+        if (r == NULL) {
+            return 0;
+        } else {
+            return 1 + max(height(r->left), height(r->right));
+        }
+    }
+    // second min
+    int secondMin(Node *r) {}
+    // second max
+    int secondMax(Node *r) {}
+} bst;
+int main() {
+    Node *t = bst.addNode(NULL, 10);
+    t = bst.addNode(t, 6);
+    t = bst.addNode(t, 15);
+    t = bst.addNode(t, 3);
+    t = bst.addNode(t, 8);
+    t = bst.addNode(t, 20);
+    bst.inorder(t);
+    cout << endl;
+    cout << "height : " << bst.height(t) << endl;
+    cout << "second min : " << bst.secondMin(t) << endl;
+    cout << "second max : " << bst.secondMax(t) << endl;
+}
---
+++ INTERNALS 2/radixsort.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+void countSort(int arr[], int n, int exp) {
+    int output[n];
+    int i, count[10] = {0};
+    for (i = 0; i < n; i++) {
+        count[(arr[i] / exp) % 10]++;
+    }
+    for (i = 1; i < 10; i++) {
+        count[i] += count[i - 1];
+    }
+    for (i = n - 1; i >= 0; i--) {
+        output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+        count[(arr[i] / exp) % 10]--;
+    }
+    for (i = 0; i < n; i++) {
+        arr[i] = output[i];
+    }
+}
+void radixsort(int arr[], int n) {
+    int m = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > m) {
+            m = arr[i];
+        }
+    }
+    for (int exp = 1; m / exp > 0; exp *= 10) {
+        countSort(arr, n, exp);
+    }
+}
+int main() {
+    int n;
+    cout << "Enter number of elements : ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter elements : ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    radixsort(arr, n);
+    cout << "After sorting : ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+}
